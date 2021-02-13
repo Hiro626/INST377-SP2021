f75aa834e23f50e8b0bde551c034bfcbb2ce3cb2
Takahiro Kawasaki <63230525+Hiro626@users.noreply.github.com>
2021-02-13 12:22:32 -0500

    change

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,16 +1,7 @@
 /* Put your javascript in here */
-let i = 1;
-
 const imageArray = ["onigiri_1", "onigiri_2", "onigiri_3", "onigiri_4",
     "roll_1", "roll_2", "roll_3"];
 
-let li = imageArray.forEach() 
-    
-carousel.querySelectorAll('li') {
-    li.style.position = 'relative';
-    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-    i++;
-}
 
 let width = 130; 
 let count = 3; 
