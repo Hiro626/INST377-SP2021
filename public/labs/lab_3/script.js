1bca86d7875f5841160814e34cc04e442146e57c
Takahiro Kawasaki <63230525+Hiro626@users.noreply.github.com>
2021-02-13 13:43:45 -0500

    changed colors

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -9,13 +9,13 @@ let list = carousel.querySelector('ul');
 let listElems = carousel.querySelectorAll('li');
 let position = 0; 
 
-carousel.querySelector('.prev').onclick = function () {
+carousel.querySelector('.prev').onclick = function() {
     position += width * count;
     position = Math.min(position, 0)
     list.style.marginLeft = position + 'px';
 };
 
-carousel.querySelector('.next').onclick = function () {
+carousel.querySelector('.next').onclick = function() {
     position -= width * count;
     position = Math.max(position, -width * (listElems.length - count));
     list.style.marginLeft = position + 'px';
