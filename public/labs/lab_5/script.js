385236b4f713b4033fdf2a49ece8287314985375
Takahiro Kawasaki <63230525+Hiro626@users.noreply.github.com>
2021-03-12 15:09:13 -0500

    lab5

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,11 +1,48 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
+  const mymap = L.map('mapid').setView([51.508, -76.9378], 13);
+  L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}' {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
   return map;
 }
 
 async function dataHandler(mapObjectFromFunction) {
   // use your assignment 1 data handling code here
   // and target mapObjectFromFunction to attach markers
+  const formInput = document.querySelector('#search-form');
+  const search = document.querySelector('#search')
+  const targetList = document.querySelector('.target-list');
+  const request = await fetch('/api');
+  const data = await request.json();
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const filtered = data.filter((record) => record.zip.includes(search.value));
+    
+    filtered.forEach((item)) => {
+      event.preventDefault();
+      const filtered = data.filter((record) => record.zip.includes(search.value));
+      filtered.forEach((item)) => {
+        const longLat = item.geocoded_column_1.coordinates;
+        const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromMapFunctio);
+
+        const appendItem = document.createElement('li');
+        appendItem.classList.add('block');
+        appendItem.classList.add('list-item');
+        appendItem.innerHTML = '<div class = ''>'
+        
+      }};
+
+      const {coordinates} = topFive[0]?.geocoded_column_1;
+      mapObjectFromFunction.panTo([coordinates[1],coordinates[0]], 0);
+  });
 }
 
 async function windowActions() {
